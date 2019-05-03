b5b378bfe69c3e3f5407ab19059d8b0ba510790f
egg <lmn1992425@126.com>
2019-05-04 02:30:08 +0800

    sms controller

---
+++ tensquare_sms/src/main/java/com/tensquare/sms/controller/SmsController.java
@@ -0,0 +1,16 @@
+package com.tensquare.sms.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短信controller
+ */
+@RestController
+@RequestMapping("/sms")
+@CrossOrigin
+public class SmsController {
+
+
+}
